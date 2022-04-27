08fa2b5482df9879c27775c7752e957733437a33
adammorelli <morelliadamoflavio@turr.hu>
2022-04-27 11:11:31 +0200

    ikon feltöltése

--- sikidomok.py
+++ sikidomok.py
@@ -1,3 +1,17 @@
 #Síkidomok v0.1
-
 from tkinter import *
+
+foablak = Tk()
+foablak.title("Síkidomok")
+foablak.geometry("1024x768")
+abl_szelesseg = 1024
+abl_magassag = 768
+kep_szelesseg = foablak.winfo_screenwidth()
+kep_magassag = foablak.winfo_screenheight()
+center_x = int(kep_szelesseg/2 - abl_szelesseg / 2)
+center_y = int(kep_magassag/2 - abl_magassag / 2)
+foablak.resizable(False, False)
+foablak.iconphoto(True, PhotoImage(file='/kellekek/negyzetikon.png'))
+
+
+foablak.mainloop()
